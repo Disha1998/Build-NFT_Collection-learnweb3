93c89291ab63e03df539e781c2fcb70b0631da73
Disha1998 <dishasathavara1998@gmail.com>
2022-12-13 12:02:31 +0530

    completed nft collection

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+  const WHITELIST_CONTRACT_ADDRESS = "0x7558B36406D1a7AaA95DD0240d9f056dc3Fc2018";
+
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-20-sophomore.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
